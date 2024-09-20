7e93d76c9227f9267bfa1098c137223bcaef811e
zkyko <168230970+zkyko@users.noreply.github.com>
2024-09-20 04:44:29 -0500

    Add files via upload

---
+++ src/components/Review.jsx
@@ -0,0 +1,109 @@
+/**
+ * @copyright 2024 codewithsadee
+ * @license Apache-2.0
+ */
+
+
+/**
+ * Node modules
+ */
+import gsap from 'gsap';
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useGSAP } from '@gsap/react';
+
+
+/**
+ * Register gsap plugins
+ */
+gsap.registerPlugin(useGSAP, ScrollTrigger);
+
+
+/**
+ * Components
+ */
+import ReviewCard from "./ReviewCard";
+
+
+const reviews = [
+  {
+    content: '.',
+    name: '',
+    imgSrc: '/images/people-1.jpg',
+    company: ''
+  },
+  {
+    content: '',
+    name: '',
+    imgSrc: '/images/people-2.jpg',
+    company: ''
+  },
+  {
+    content: '',
+    name: '',
+    imgSrc: '/images/people-3.jpg',
+    company: ''
+  },
+  {
+    content: '',
+    name: '',
+    imgSrc: '/images/people-4.jpg',
+    company: ''
+  },
+  {
+    content: '',
+    name: '',
+    imgSrc: '/images/people-5.jpg',
+    company: ''
+  },
+  {
+    content: '',
+    name: '',
+    imgSrc: '/images/people-6.jpg',
+    company: ''
+  }
+];
+
+
+const Review = () => {
+
+  useGSAP(() => {
+    gsap.to('.scrub-slide', {
+      scrollTrigger: {
+        trigger: '.scrub-slide',
+        start: '-200% 80%',
+        end: '400% 80%',
+        scrub: true
+      },
+      x: '-1000'
+    })
+  });
+
+  return (
+    <section
+      id="reviews"
+      className="section overflow-hidden"
+    >
+      <div className="container">
+
+        <h2 className="headline-2 mb-8 reveal-up">
+          What our customers say
+        </h2>
+
+        <div className="scrub-slide flex items-stretch gap-3 w-fit">
+          {reviews.map(({ content, name, imgSrc, company }, key) => (
+            <ReviewCard
+              key={key}
+              name={name}
+              imgSrc={imgSrc}
+              company={company}
+              content={content}
+            />
+          ))}
+        </div>
+
+      </div>
+    </section>
+  )
+}
+
+export default Review
